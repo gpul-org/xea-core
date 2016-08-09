eef8498388c672b25344a3f6fd8c05166e4ed4f6
Santiago Saavedra <ssaavedra@gpul.org>
2016-08-09 20:14:29 +0200

    Add namespace to jwt_knox URLs

--- xea_core/urls.py
+++ xea_core/urls.py
@@ -19,7 +19,7 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
 
-    url(r'api/auth/jwt/', include('jwt_knox.urls')),
+    url(r'api/auth/jwt/', include('jwt_knox.urls', namespace='jwt_knox')),
 
     url(r'^api/', include('api.urls')),
 ]

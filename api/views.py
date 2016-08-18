63ec7d085a8bf545f2ec80494c8ec886570a7668
Santiago Saavedra <ssaavedra@gpul.org>
2016-08-18 20:26:23 +0200

    Add API Root

--- api/views.py
+++ api/views.py
@@ -3,6 +3,7 @@ from rest_framework.decorators import (api_view, permission_classes,
 from rest_framework.permissions import AllowAny
 from rest_framework.renderers import CoreJSONRenderer
 from rest_framework.response import Response
+from rest_framework.reverse import reverse
 from rest_framework.schemas import SchemaGenerator
 from rest_framework_swagger.renderers import (OpenAPIRenderer,
     SwaggerUIRenderer)
@@ -14,3 +15,16 @@ from rest_framework_swagger.renderers import (OpenAPIRenderer,
 def schema_view(request):
     generator = SchemaGenerator(title='XEA API')
     return Response(generator.get_schema(request=request))
+
+@api_view()
+def api_root(request, format=None):
+    return Response({'message': 'Welcome to XEA'})
+    return Response({
+        '_self': reverse('root', request=request, format=format),
+        '_links': {
+            'docs': reverse('swagger-ui', request=request, format=format),
+            'auth': {
+                'jwt': reverse('jwt_knox-get-token', request=request, format=format),
+            },
+        },
+    })

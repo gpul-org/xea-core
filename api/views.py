fd1c0a6a386b1861f9d0caf75d548de49a209e33
Santiago Saavedra <ssaavedra@gpul.org>
2016-08-18 21:55:38 +0200

    Patch SchemaGeneration to include API documentation

--- api/views.py
+++ api/views.py
@@ -1,30 +1,37 @@
 from rest_framework.decorators import (api_view, permission_classes,
-    renderer_classes)
+                                       renderer_classes)
 from rest_framework.permissions import AllowAny
 from rest_framework.renderers import CoreJSONRenderer
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
-from rest_framework.schemas import SchemaGenerator
 from rest_framework_swagger.renderers import (OpenAPIRenderer,
-    SwaggerUIRenderer)
+                                              SwaggerUIRenderer)
+
+from .schemas import SchemaGenerator
 
 
 @api_view()
 @permission_classes((AllowAny, ))
-@renderer_classes((OpenAPIRenderer, CoreJSONRenderer, SwaggerUIRenderer, ))
+@renderer_classes((OpenAPIRenderer,
+                   CoreJSONRenderer,
+                   SwaggerUIRenderer, ))
 def schema_view(request):
     generator = SchemaGenerator(title='XEA API')
     return Response(generator.get_schema(request=request))
+
 
 @api_view()
 def api_root(request, format=None):
     return Response({'message': 'Welcome to XEA'})
     return Response({
-        '_self': reverse('root', request=request, format=format),
+        '_self': reverse(
+            'root', request=request, format=format),
         '_links': {
-            'docs': reverse('swagger-ui', request=request, format=format),
+            'docs': reverse(
+                'swagger-ui', request=request, format=format),
             'auth': {
-                'jwt': reverse('jwt_knox-get-token', request=request, format=format),
+                'jwt': reverse(
+                    'jwt_knox-get-token', request=request, format=format),
             },
         },
     })

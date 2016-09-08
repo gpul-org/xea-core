749f8e0ff69094f82d52031a72c236666ed9400a
Santiago Saavedra <ssaavedra@gpul.org>
2016-09-08 16:59:00 +0200

    Use DRF reverse instead of Django's
    We had to change reverse anyway because Django 1.10 deprecates the
    django.core.urlresolvers path in favor of django.urls. But it seems the
    tests run successfully with rest_framework.reverse instead, and we are
    creating a rest_framework app, we should be using rest_framework
    packages where possible.

--- jwt_knox/tests.py
+++ jwt_knox/tests.py
@@ -1,8 +1,8 @@
 from rest_framework.test import APITestCase
 
 from django.contrib.auth.models import User
-from django.core.urlresolvers import reverse
 from rest_framework import status
+from rest_framework.reverse import reverse
 
 
 class APIAuthTest(APITestCase):
